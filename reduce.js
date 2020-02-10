b2048195ade5ea3d59961cb7d4d7664561a4d899
Renée Cruz <reneebasscruz@gmail.com>
2020-02-09 21:03:18 -0500

    corrected spelling of necessary

--- reduce.js
+++ reduce.js
@@ -11,7 +11,7 @@ function reduce(array, combiningFunction, startingIndex) {
   console.log(reduce([1,2,3,4], (a, b) => a + b, 0));
   // 10 
 
-  // reduce method .reduce() is called on an array and accepts a callback function that is a reducer, or a combining function that performs the operation (summation, or totaling) and it accepts four parameters, two neccessary (accumulator and current value) and two optional (starting index and array its being performed upon)
+  // reduce method .reduce() is called on an array and accepts a callback function that is a reducer, or a combining function that performs the operation (summation, or totaling) and it accepts four parameters, two necessary (accumulator and current value) and two optional (starting index and array its being performed upon)
 
 array = [1, 2, 3, 4]
 
