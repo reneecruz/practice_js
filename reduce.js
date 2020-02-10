584c9f779d612206f36383f37e3eada1cd30e306
Renée Cruz <reneebasscruz@gmail.com>
2020-02-09 20:59:41 -0500

    added .reduce() method in ES5 and ES6

--- reduce.js
+++ reduce.js
@@ -10,3 +10,17 @@ function reduce(array, combiningFunction, startingIndex) {
   
   console.log(reduce([1,2,3,4], (a, b) => a + b, 0));
   // 10 
+
+  // reduce method .reduce() is called on an array and accepts a callback function that is a reducer, or a combining function that performs the operation (summation, or totaling) and it accepts four parameters, two neccessary (accumulator and current value) and two optional (starting index and array its being performed upon)
+
+array = [1, 2, 3, 4]
+
+// ES5 Syntax
+
+array.reduce(function(accumulator, currentValue) {
+  return accumulator + currentValue
+}, 0)
+
+// ES6 Arrow Function Syntax
+
+array.reduce(( accumulator, currentValue ) => accumulator + currentValue, 0 )
